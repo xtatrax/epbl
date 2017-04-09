5ce595708fb5d23571a60a8d7a8d50df7b76a868
shigihara <shigihara@fuzz.co.jp>
2017-04-09 13:04:16 +0900

    メッセージをログに。

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <time.h>
+
+void print_log(std::string file, const char *format, ...){
+    char tmp[2048];
+    va_list arg;
+    va_start(arg, format);
+    vsprintf(tmp, format, arg);
+    va_end(arg);
+    time_t now = time(NULL);
+    struct tm *pnow = localtime(&now);
+
+    std::ofstream outputfile(file);
+    outputfile << pnow->tm_year + 1900 << "/" << pnow->tm_mon + 1 << "/" << pnow->tm_mday <<
+    "/" << pnow->tm_hour << ":" << pnow->tm_min << ":" <<  pnow->tm_sec << tmp;
+    outputfile.close();
+}
+
 
 std::string getDataPath(){
     HKEY hKey;
@@ -22,7 +39,7 @@ std::string getDataPath(){
         FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
             NULL, GetLastError(), LANG_USER_DEFAULT,
             (LPTSTR)&lpBuffer, 0, NULL);
-        MessageBox(NULL, lpBuffer, "RegOpenKeyEx Error Message", MB_ICONHAND | MB_OK);
+        print_log("c:\epbllog.txt","RegOpenKeyEx Error Message (%d) : %s", result , lpBuffer);
         LocalFree(lpBuffer);
         return NULL;
     }
@@ -44,29 +61,22 @@ std::string getDataPath(){
 
 void pExec(char* str, int flag)
 {
+    print_log(getDataPath() + "log.txt", "exec : %s", str);
+
 	PROCESS_INFORMATION pi;
 	STARTUPINFO si;
     int ret;
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
-    if(ret = CreateProcess(nullptr, str, nullptr, nullptr, FALSE, NORMAL_PRIORITY_CLASS, nullptr, nullptr, &si, &pi)){
+    ret = CreateProcess(nullptr, str, nullptr, nullptr, FALSE, NORMAL_PRIORITY_CLASS, nullptr, nullptr, &si, &pi);
+    if(ret >= 2){
         LPTSTR lpBuffer = NULL;
         FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
             NULL, GetLastError(), LANG_USER_DEFAULT,
             (LPTSTR)&lpBuffer, 0, NULL);
-        char msg[1024] ="";
-        char retStr[8]="";
-        itoa(ret, retStr,10);
-        strcat(msg, lpBuffer);
-        strcat(msg, "\r\n");
-        strcat(msg, str);
-        strcat(msg, "\r\n");
-        strcat(msg, retStr);
-        char cBuff[256] = "";
-        GetConsoleTitle(cBuff, 256);
-        MessageBox(FindWindow(NULL, cBuff), msg, "CreateProcess Error Message", MB_ICONHAND | MB_OK);
+        print_log(getDataPath() + "log.txt" ,"CreateProcess Error Message (%d) : %s", ret, lpBuffer);
         LocalFree(lpBuffer);
-        CopyFile("c:\\Windows\\system32\\LogonUIOriginal.exe", "c:\\Windows\\system32\\LogonUI.exe", FALSE);
+        //CopyFile("c:\\Windows\\system32\\LogonUIOriginal.exe", "c:\\Windows\\system32\\LogonUI.exe", FALSE);
     }
     //ÉfÅ[É^ÇéÊìæ
 
@@ -82,7 +92,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
 
     std::string data = getDataPath();
-	char str[64] = {};
+	char str[128] = {};
 
 	char* s = " ";
     std::string exec = data + "\\bin\\LogonUI.exe";
@@ -94,26 +104,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		strcat(str, s);
 		strcat(str, __argv[i]);
 	}
+    pExec(str, 0);
 
 
     std::ifstream ifs(data + "\\config\\execlist.conf");
     std::string execstr;
     if (ifs.fail())
     {
-        char cBuff[256] = "";
-        GetConsoleTitle(cBuff, 256);
-
-        MessageBox(
-            FindWindow(NULL, cBuff),
-            TEXT("file not found"),
-            TEXT("error"),
-            MB_OK | MB_ICONWARNING);
+        print_log(getDataPath() + "log.txt", "file not found : %s", (data + "\\config\\execlist.conf").c_str());
     }
     while (std::getline(ifs, execstr))
     {
-        pExec((char*)execstr.c_str(), 1);
+        pExec((char*)execstr.c_str(), 0);
     }
-    pExec(str, 0);
 
     return 0;
 }
